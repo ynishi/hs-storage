f7465618ca804d47a3631d0a2678c6fdd4f873d7
Yutaka Nishimura <ytk.nishimura@gmail.com>
2021-10-26 00:34:28 +0900

    add check app for smart and zfs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( check
+  ) where
+
+import qualified Control.Foldl  as F
+import           Control.Monad
+import qualified Data.Text      as T
+import           Turtle
+import           Turtle.Prelude
+
+run cmd = fold (inshell cmd empty) F.list
+
+run' cmd = shell cmd empty
+
+pickBy f = map (head . T.words) . filter f . map lineToText
+
+check :: IO ()
+check = do
+  disks <- run "lsblk"
+  let disks' = pickBy (T.isInfixOf "disk") disks
+  forM_ disks' $ \disk ->
+    run' ("smartctl -i /dev/" <> disk) .&&.
+    run' ("smartctl -t long /dev/" <> disk) .&&.
+    run' ("echo smart tested:" <> disk)
+  pools <- run "zpool list -H -o health,name"
+  let pools' = pickBy (T.isSuffixOf "ONLINE") pools
+  forM_ pools' $ \pool ->
+    run' ("zpool scrub " <> pool) .&&. run' ("echo scrubed:" <> pool)
